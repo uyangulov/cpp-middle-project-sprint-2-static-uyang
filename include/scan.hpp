e62267bbd20a2cb962da05c1f5daf9c81aa9f82f
kirill.uyangulov <kirill.uyangulov@gmail.com>
2026-01-09 19:05:58 +0000

    remove redundant comments and add remove_cv to parsed types

--- include/scan.hpp
+++ include/scan.hpp
@@ -3,6 +3,7 @@
 #include <cstddef>
 #include <functional>
 #include <tuple>
+#include <type_traits>
 #include <utility>
 
 #include "format_string.hpp"
@@ -12,36 +13,23 @@
 namespace stdx
 {
 
-// template <details::format_string fmt, details::fixed_string source, typename
-// T1,
-//           typename T2>
-// consteval details::scan_result<T1, T2> scan()
-// {
-//     return details::scan_result<T1, T2>(
-//         std::make_tuple(*stdx::details::parse_input<0, fmt, source, T1>(),
-//                         *stdx::details::parse_input<1, fmt, source, T2>()));
-// }
-
-// template <details::format_string fmt, details::fixed_string source, typename
-// T> consteval details::scan_result<T> scan()
-// {
-//     return details::scan_result<T>(
-//         std::make_tuple(*stdx::details::parse_input<0, fmt, source, T>()));
-// }
-
 template <details::format_string fmt, details::fixed_string source,
           typename... Ts, size_t... Is>
-consteval details::scan_result<Ts...> scan_impl(std::index_sequence<Is...>)
+consteval details::scan_result<std::remove_cv_t<Ts>...>
+
+scan_impl(std::index_sequence<Is...>)
 {
-    return details::scan_result<Ts...>(
-        std::make_tuple(*stdx::details::parse_input<Is, fmt, source, Ts>()...));
+    return {std::make_tuple(
+        *stdx::details::parse_input<Is, fmt, source,
+                                    std::remove_cv_t<Ts>>()...)};
 }
 
 template <details::format_string fmt, details::fixed_string source,
           typename... Ts>
-consteval details::scan_result<Ts...> scan()
+consteval details::scan_result<std::remove_cv_t<Ts>...> scan()
 {
-    return scan_impl<fmt, source, Ts...>(std::index_sequence_for<Ts...>{});
+    return scan_impl<fmt, source, std::remove_cv_t<Ts>...>(
+        std::index_sequence_for<Ts...>{});
 }
 
 }  // namespace stdx
